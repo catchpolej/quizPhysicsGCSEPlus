6ec925eb2a3bd17d6b85f63223511e4488732b79
Jeremy Catchpole <jscatchpole@gmail.com>
2026-03-26 15:44:28 +0100

    Add files via upload

---
+++ spac/hint.js
@@ -0,0 +1,71 @@
+function HintXX(){alert ("The hint is below this button.")}
+function Hint01(){alert ("Try using the mnemonic My Very Easy Method (Always) Just Speeds Up Naming Planets to remember the order of the planets.")}
+function Hint02(){alert ("The Inner planets are the planets closest to the Sun.")}
+function Hint03(){alert ("The Outer planets are the planets which are much further away from the Sun and Inner planets.")}
+function Hint04(){alert ("Try using the mnemonic My Very Energetic Maiden Aunt Just Swam Under North Pier to remember the order of the planets and where the Asteroids lie.")}
+function Hint05(){alert ("Planets are not hot enough to shine by their own light.  It is possible to see them because they reflect light from the Sun.")}
+function Hint06(){alert ("The Sun produces heat and light from the fuel hydrogen.  The hydrogen nuclei are transformed into helium nuclei during a nuclear process, releasing enormous amounts of energy.")}
+function Hint07(){alert ("If the Sun produced its energy from chemical reactions, it would have used up all of its mass many millions of years ago.")}
+function Hint08(){alert ("Most planets contain very little hydrogen, but where there is large amounts present; such as in Jupiter, the temperature is not high enough to initiate and sustain nuclear reactions.")}
+function Hint09(){alert ("Your eyes can only detect visible light electromagnetic waves.  The nuclear reactions and heat of the Sun produce all types of electromagnetic waves.")}
+function Hint10(){alert ("One theory of how our Solar System was formed, is that the Sun and planets were formed from other stars.")}
+function Hint11(){alert ("The four Inner planets are Mercury, Venus, Earth and Mars.  They are all similar in size and do show evidence of volcanic activity.")}
+function Hint12(){alert ("The five Outer planets are Jupiter, Saturn, Uranus, Neptune and Pluto.  They are mostly gas composed.")}
+function Hint13(){alert ("The temperature on Earth rarely exceeds 40°C and occasionally falls below freezing.  The average Earth - Sun distance is 150 million Km.")}
+function Hint14(){alert ("An electric force is the force between two electrically charged particles.  A nuclear force is the force of attraction between all the particles of an atomic nucleus.")}
+function Hint15(){alert ("The closer you get to the star, the stronger the force of attraction.")}
+function Hint16(){alert ("The size of the force gravity follows the Inverse Square Law.  E.g. If you double the distance from the Earth, then the size of the force will decrease by a factor of 4 (2²).")}
+function Hint17(){alert ("The size of the force gravity follows the Inverse Square Law.  E.g. If you treble the distance from the Earth, then the size of the force will decrease by a factor of 9 (3²).")}
+function Hint18(){alert ("The size of the force gravity follows the Inverse Square Law.  E.g. If you half the distance from the Earth, then the size of the force will increase by a factor of 4 (2²).")}
+function Hint19(){alert ("The bigger the size of the body, the stronger its gravitational field.  This implies that more fuel will be required to escape from a larger body, than from a smaller one.  The rocket will not require as much fuel to escape from an orbit, as it did to get into that orbit.  It will use no fuel travelling between the Earth and the Moon.")}
+function Hint20(){alert ("The closer you get to a body, the stronger the force of attraction.")}
+function Hint21(){alert ("A galaxy is a star system containing a group of many millions of stars.  The Solar System is the system containing the Sun and the bodies held in its gravitational field.  These include the Planets, Asteroids and Comets.")}
+function Hint22(){alert ("An asteroid is unlikely to be visible to the naked eye and a meteor would be seen to move across the constellation quite fast.")}
+function Hint23(){alert ("It takes the Moon roughly 28 days to orbit the Earth.")}
+function Hint24(){alert ("An eclipse of the Moon occurs when the Earth moves between the Sun and the Moon.")}
+function Hint25(){alert ("An eclipse of the Sun occurs when the Moon moves between the Sun and the Earth.")}
+function Hint26(){alert ("When you can see less than half of the illuminated Moon, it is called a 'Crescent Moon'.  When you can see more than half of the illuminated Moon, it is called a 'Gibbous Moon'.")}
+function Hint27(){alert ("The drag on the Earth induced by the Moon will cause the Earth to slow down slightly.")}
+function Hint28(){alert ("Asteroids are very different to meteorites.  They stay in their orbits and do not come near Earth or enter its atmosphere.")}
+function Hint29(){alert ("Meteorites do not orbit the Sun.  They just come flying into the Solar System.")}
+function Hint30(){alert ("The planets and asteroids orbit the Sun in orbits which are more circular than that of a comet.")}
+function Hint31(){alert ("The pull of gravity by the Sun will speed up a comet as it gets closer, and will slow it down as it gets further away from the Sun.")}
+function Hint32(){alert ("At present, satellites are not used to transfer solar energy from the Sun to the Earth.")}
+function Hint33(){alert ("Geostationary satellites stay above the same point on the Earth's surface, which makes them ideal for telecommunications.")}
+function Hint34(){alert ("For a satellite to be in a geostationary orbit, it must be launched at a precise velocity otherwise it will not remain over a single point on the Earth's surface.")}
+function Hint35(){alert ("Low Polar orbit satellites are ideal for taking weather and spying photographs.  These satellites orbit over both poles, whilst the Earth rotates at 90° to this direction beneath them.")}
+function Hint36(){alert ("Water vapour in the Earth's atmosphere greatly reduces the amount of light that reaches us from the stars and planets.")}
+function Hint37(){alert ("It will be many millions of years before a space probe reaches the end of our galaxy or any other galaxies.")}
+function Hint38(){alert ("It takes the Earth one day to complete one full rotation.")}
+function Hint39(){alert ("Places inside the Arctic circle experience 24 hours of sunlight in mid summer and 24 hours of darkness in mid winter.")}
+function Hint40(){alert ("It takes the Earth one year to orbit the Sun.")}
+function Hint41(){alert ("At 'A', the Northern Hemisphere is tilted towards the Sun, giving it long days in the Sun.  At 'C', the Northern Hemisphere is tilted away from the Sun, giving it long days in darkness.  At 'B' and 'D', the axis is tilted broadside to the Sun, so everywhere has 12 hours of sunlight and 12 hours of darkness.")}
+function Hint42(){alert ("The closer you get to the star, the faster the planet will have to orbit to remain in orbit around the star.")}
+function Hint43(){alert ("The force of gravity is responsible for the tides experienced on Earth.")}
+function Hint44(){alert ("During a spring tide, the Sun, Earth and Moon are all lined up.  This means a spring tide can be caused by two phases of the Moon.")}
+function Hint45(){alert ("We experience tides on the Earth due to the creation of these bulges.")}
+function Hint46(){alert ("At Full Moon and New Moon, the Sun and the Moon's gravitational forces add up to give very high and very low tides.")}
+function Hint47(){alert ("Our Sun and Solar System belong to a group of stars known as the Milky Way.")}
+function Hint48(){alert ("Our Sun and Solar System are positioned towards the end of a spiral arm, within the Milky Way Galaxy.")}
+function Hint49(){alert ("A light year is the distance light can travel in one year.")}
+function Hint50(){alert ("The human body can only cope with relatively small accelerations and decelerations.")}
+function Hint51(){alert ("To calculate the distance travelled in one light year, work out the number of seconds in a year and multiply this by the speed of light.")}
+function Hint52(){alert ("Calculate the time in seconds and mutiply by speed of light.")}
+function Hint53(){alert ("Neutron stars and Black holes are only created by very large stars.")}
+function Hint54(){alert ("White Dwarfs and Black Dwarfs are only created by small stars like our Sun.")}
+function Hint55(){alert ("The high temperatures within a star means that its atoms and molecules are moving at very high speeds and therefore possess a great deal of kinetic energy.")}
+function Hint56(){alert ("As the Sun changes into a Red Giant, it will grow considerably larger.  This is because the gravitational force holding the Sun together is no longer strong enough to overcome the outward radiation pressure, therefore it will expand.")}
+function Hint57(){alert ("These explosions are believed to be caused when a star runs out of hydrogen and contracts under its own gravitational field.  This contraction causes sufficiently high temperatures in the star's interior for thermonuclear reactions to occur.  The star will collapse inwards, increasing its speed of rotation and ultimately flinging a large proportion of its matter into space.")}
+function Hint58(){alert ("Heavy elements are only created in the final stages of a big star's Supernova.  The presence of these heavier elements in the Sun and inner planets is clear evidence that they were formed from a dying star.")}
+function Hint59(){alert ("The gravity is so strong within Neutron stars, White Dwarfs and Black Dwarfs, that it even crushes atoms.")}
+function Hint60(){alert ("If a galaxy is moving away from us, its spectral lines will be shifted towards the red end of the spectrum.")}
+function Hint61(){alert ("The fact that the light is shifted towards the red end of the spectrum means that the galaxies are moving away from us.  The further away a galaxy is, the greater this red shift.")}
+function Hint62(){alert ("As the Universe expands and cools, the background radiation 'cools' and drops in frequency.  This is very strong evidence for an initial Big Bang.")}
+function Hint63(){alert ("The Steady State theory of the Universe explains the apparent expansion of the Universe by suggesting that matter is being created in the spaces as the Universe expands.")}
+function Hint64(){alert ("The background radiation 'cools' and drops in frequency which fits in better with the theory of an initial Big Bang.")}
+function Hint65(){alert ("All the matter in the Universe must have been compressed into a very small space and then it exploded.  This initial explosion being the Big Bang and the expansion is still going on today.")}
+function Hint66(){alert ("Without gravity the Universe would expand at the same rate forever.  The attraction between all the mass in the Universe is believed to slow its expansion down.")}
+function Hint67(){alert ("It is easy to measure how fast galaxies are separating, but calculating how much mass there is in the Universe is more difficult because most of the matter is invisible; like Black Holes.  This is why it is hard to predict the future of the Universe.")}
+function Hint68(){alert ("Our Sun is too small to become a Supernova or a Black Hole.")}
+function Hint69(){alert ("If there is too little mass in the Universe to slow its expansion down, then it could expand forever with the Universe becoming more and more spread out.")}
+function Hint70(){alert ("The temperature will rise to a maximum and then level out.")}
---
+++ wave/hint.js
@@ -0,0 +1,100 @@
+function Hint01(){alert ("Y is wavelength because the distance shown is from a peak to a peak, which is a full cycle of the wave")}
+function Hint02(){alert ("The amplitude of a wave is the vertical distance from a peak to the central horizontal line")}
+function Hint03(){alert ("Longitudinal waves vibrate along the direction of travel of the wave.  Transverse waves vibrate perpendicular to the direction of travel of the wave")}
+function Hint04(){alert ("A transverse wave has oscillations perpendicular to its direction of travel")}
+function Hint05(){alert ("Transverse waves are waves that vibrate at 90 degrees to their direction of travel.  All electromagnetic waves are classified as transverse.  Longitudinal waves vibrate along the same line as the direction that the wave travels")}
+function Hint06(){alert ("Waves transfer energy from one place to another without there being a transfer of matter.")}
+function Hint07(){alert ("Generally the denser the medium, the faster sound travels through it.")}
+function Hint08(){alert ("Too much loud noise will damage your hearing.")}
+function Hint09(){alert ("Insulation in buildings will reduce noise pollution.")}
+function Hint10(){alert ("Soft surfaces such as curtains and carpets absorb sound quickly and stop it echoing.")}
+function Hint11(){alert ("An amplifier is used to change the loudness of a sound.")}
+function Hint12(){alert ("Changing the amplitude should not effect the pitch of the sound.")}
+function Hint13(){alert ("Low frequency sound waves sound low pitched.")}
+function Hint14(){alert ("When you increase the tension on a wire, it will emit a higher pitched note when plucked.")}
+function Hint15(){alert ("The amplitude is a measure of how loud a sound is.  It is indicated on the graphs by the height of the wave above its zero displacement - the horizontal white line.")}
+function Hint16(){alert ("The graph with the greatest number of waves per unit time has the highest frequency.")}
+function Hint17(){alert ("One wavelength is the distance from one wave crest to the next.")}
+function Hint18(){alert ("An amplifier is an electronic device used to increase the strength of the current fed into it.")}
+function Hint19(){alert ("Alternative methods to using ultrasound for cleaning mechanisms would either damage the equipment or would require it to be dismantled first.")}
+function Hint20(){alert ("Ultrasound is a series of high frequency vibrations.  As they are a form of sound wave, the denser the object, the faster the wave travels through it.")}
+function Hint21(){alert ("X-rays and gamma rays would be very dangerous to a foetus.")}
+function Hint22(){alert ("The bats use SONAR.  They detect the echoes created from the ultrasound that they emit.")}
+function Hint23(){alert ("Use the formula (s = d / t).")}
+function Hint24(){alert ("Use the formula (Velocity = Frequency x Wavelength).  Remember to convert frequency into hertz and wavelength into metres.")}
+function Hint25(){alert ("The time period of a wave is the time the wave takes to complete one cycle.  Rearrange the formula (f = 1 / T) to form (T = 1 / f).  Then solve.")}
+function Hint26(){alert ("Rearrange the formula (s = d / t) to form (d = st).  Then solve.  Remember to divide your answer by 2, otherwise the distance you have is to the wall and then back again.")}
+function Hint27(){alert ("The frequency of the waves is defined as the number of complete waves generated per second.")}
+function Hint28(){alert ("Light travels at 300,000,000 metres per second.  Sound travels at 330 metres per second.")}
+function Hint29(){alert ("Study diagram ‘B’.  Imagine that the reflected wave is the incident wave.  This would then produce straight reflected waves.")}
+function Hint30(){alert ("When light reflects off an object all at the same angle, you get a clear reflection.")}
+function Hint31(){alert ("The angle of incidence is the angle between the normal to the mirror and the light ray.")}
+function Hint32(){alert ("If you close your left eye when looking in a mirror, your image seems to close the right eye.  This is an example of lateral inversion.")}
+function Hint33(){alert ("A real image is one which can be produced on a screen and is formed by rays that actually pass through it.  A virtual image cannot be formed on a screen and is produced by rays which seem to come from it, but do not pass through it.")}
+function Hint34(){alert ("The image in a plane (flat) mirror is as far behind the mirror as the object is in front.")}
+function Hint35(){alert ("The image formed on the retina of the human eye is inverted.")}
+function Hint36(){alert ("The image formed in a pinhole camera by a close and upright object will be inverted.")}
+function Hint37(){alert ("If the screen is moved closer to the pinhole, the light will have less distance in which it diverges.")}
+function Hint38(){alert ("The human eye and a camera both contain converging lenses and they both form inverted images.")}
+function Hint39(){alert ("Both a camera and a projector will produce an inverted image.")}
+function Hint40(){alert ("When the image of what is being seen forms in front of the retina, this condition is called short sight.  In order to make the image form on the retina, you need to place a lens in front of the eye that will spread the light rays further apart.")}
+function Hint41(){alert ("Reflection is the change in direction of a wave due to it bouncing off a boundary between two media.")}
+function Hint42(){alert ("Since the light is passing into a denser medium, it will bend towards the normal, but the frequency will remain the same.")}
+function Hint43(){alert ("The light is going from a medium of low density, to one of a high density.  Its frequency will remain unchanged and it will slow down.")}
+function Hint44(){alert ("When the light ray enters the more dense medium; the water, it gets bent towards the normal.")}
+function Hint45(){alert ("As waves in a ripple tank enter shallower water, they are bent towards the normal.")}
+function Hint46(){alert ("When something spreads out, it is said to disperse.")}
+function Hint47(){alert ("When a ray of light passes from one medium into a less dense one, the light will bend away from the normal.")}
+function Hint48(){alert ("When light travels through a prism, it gets dispersed.  Red light gets dispersed the least.")}
+function Hint49(){alert ("Learn the mnemonic ‘Richard Of York Gave Battle In Vain’.")}
+function Hint50(){alert ("The critical angle is the particular angle of incidence of a ray hitting a less dense medium, which results in it being refracted at 90 degrees to the normal.")}
+function Hint51(){alert ("In diagram ‘A’ most of the light passes out of the glass because the angle of incidence is less than the critical angle.  In diagram ‘B’, most of the light is refracted at 90 degrees to the normal because the angle of incidence is equal to the critical angle.  No light will leave the glass when the angle of incidence is greater than the critical angle.")}
+function Hint52(){alert ("Binoculars and periscopes use 45 degree prisms because they give a better reflection than a mirror.")}
+function Hint53(){alert ("The only disadvantage to an optic cable is that it does lose light at reflections due to imperfections in the material the light is travelling in.  However, the light signals do not need boosting as often as electrical signals.")}
+function Hint54(){alert ("Telescopes and periscopes use prisms to reflect images to the viewer.")}
+function Hint55(){alert ("The waves are not being reflected or refracted.")}
+function Hint56(){alert ("For diffraction to be at its greatest, the width of the slit should be the same as or less than the wavelength of the waves.")}
+function Hint57(){alert ("When a wave is diffracted through a narrow gap, the frequency of the wave will remain the same.")}
+function Hint58(){alert ("Diffraction is a property of all waves.  Higher frequency sounds have shorter wavelengths.")}
+function Hint59(){alert ("The wavelength of light and its amplitude can both be varied.")}
+function Hint60(){alert ("Use the mnemonic ‘Roll Me In Lead Until X-rays Go’ to remember the order of the EM spectrum.  Gamma rays are highest in the electromagnetic spectrum, followed by X-rays.  There is another type of radiation in between visible light and X-rays.")}
+function Hint61(){alert ("Use the mnemonic ‘Roll Me In Lead Until X-rays Go’ to remember the order of the EM spectrum.  Gamma radiation and X-rays are both higher in the electromagnetic spectrum than light.")}
+function Hint62(){alert ("Use the mnemonic ‘Roll Me In Lead Until X-rays Go’ to remember the order of the EM spectrum.")}
+function Hint63(){alert ("Light, X-rays and gamma rays are types of electromagnetic waves.")}
+function Hint64(){alert ("All electromagnetic waves can travel through a vacuum.")}
+function Hint65(){alert ("When absorbed, electromagnetic waves can cause two effects.")}
+function Hint66(){alert ("To receive short wavelength radio waves, it it best to be in direct sight of the transmitter because they will not bend over or around objects.")}
+function Hint67(){alert ("A type of radio wave will travel long distances because it will be reflected from the ionosphere.")}
+function Hint68(){alert ("Microwaves are used for satellite communications and for cooking with in a microwave oven.")}
+function Hint69(){alert ("It is ultra violet light that causes skin cancer.")}
+function Hint70(){alert ("Infrared and microwaves both have longer wavelengths than visible light.")}
+function Hint71(){alert ("If something is opaque, it is not transparent.  Light is a form electromagnetic radiation.")}
+function Hint72(){alert ("Infrared is felt on the skin as radiant heat.")}
+function Hint73(){alert ("Seismic waves are shock waves that travel through the Earth.")}
+function Hint74(){alert ("A Barometer measures pressure and a Voltmeter measures potential difference.")}
+function Hint75(){alert ("P-Waves are a type of seismic wave.")}
+function Hint76(){alert ("S-Waves will only travel through solids.")}
+function Hint77(){alert ("P-Waves will travel through solids and liquids.")}
+function Hint78(){alert ("S-Waves will only travel through solids, therefore about halfway down, the Earth must be liquid.  This is known as the outer core.")}
+function Hint79(){alert ("The outer core is liquid.  Seismic waves will travel faster through denser materials.")}
+function Hint80(){alert ("The paths of the waves curve due to increasing density.  This is refraction.")}
+function Hint81(){alert ("The frequency of your heartbeat is the number of beats per second.")}
+function Hint82(){alert ("Rearrange the formula (s = d / t) to form (t = d / s).  Then solve.  Remember there are 1000 metres in one kilometre.")}
+function Hint83(){alert ("There is a maximum diffraction when the wavelength of the waves is the same as or greater than the width of the slit.")}
+function Hint84(){alert ("When the light ray enters the glass block, it bends towards the normal.  When it emerges from the block, it travels in the same direction as it did when it entered.")}
+function Hint85(){alert ("All electromagnetic waves can travel through a vacuum.")}
+function Hint86(){alert ("One wavelength is the distance from one wave crest to the next.")}
+function Hint87(){alert ("Rearrange the formula (Velocity = Frequency x Wavelength) to form (Frequency = Velocity / Wavelength).  Then solve.")}
+function Hint88(){alert ("Radiographers who take X-ray pictures continuously, wear lead aprons and stand behind a lead screen to minimise their exposure.")}
+function Hint89(){alert ("Use the formula (f = 1 / T).")}
+function Hint90(){alert ("Use the mnemonic ‘Roll Me In Lead Until X-rays Go’ to remember the order of the EM spectrum.")}
+function Hint91(){alert ("First calculate the speed of the waves by using (s = d / t).  Then rearrange (Velocity = Frequency x Wavelength) to form (Frequency = Velocity / Wavelength).  Then solve.  This will give you the frequency if you remembered to convert distances to metres.")}
+function Hint92(){alert ("When the light rays pass from the object and out into the air, they get bent away from the normal.  The normal is a line at 90 degrees to the surface of the water.")}
+function Hint93(){alert ("Rearrange the formula (s = d / t) to form (d = st).  Then solve.")}
+function Hint94(){alert ("Visible light falls between Infrared and Ultra violet in the electromagnetic spectrum.")}
+function Hint95(){alert ("Rearrange the formula (s = d / t) to form (t = d / s).  Then solve.  Remember to multiply your answer by 2, otherwise the time you have is only for the sound to reach the house.")}
+function Hint96(){alert ("Gamma rays do kill bacteria.")}
+function Hint97(){alert ("The image appears to be as far behind the mirror as the object is in front.  Therefore the person's eyes appear to be 2.5m behind the mirror.  Since the person is 2.5m in front of the mirror, the person is 5m away from his image.  However, the test card appears to be an additional 0.5m away.")}
+function Hint98(){alert ("Rearrange the formula (Velocity = Frequency x Wavelength) to form (Wavelength = Velocity / Frequency).  Then solve.")}
+function Hint99(){alert ("The frequency of a sound wave determines its pitch.  The graph with the least number of waves per unit time has the lowest pitch.")}
+function Hint100(){alert ("High frequency waves have short wavelengths.")}
